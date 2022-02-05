8bb857d0cd43a85e89eb5fa9a1b3b97cbabfc708
Shadman Ahmed <shadmanchowdhury87@gmail.com>
2022-02-05 21:05:07 +0600

    Add user update functionality

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import {
   collection,
   getDocs,
   addDoc,
+  getDoc,
+  updateDoc,
+  doc,
 } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
 
 // Your firebase configuration
@@ -17,29 +20,59 @@ export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore();
 
+// Flag variables:
+var userCount = 0;
+var id;
+var editing = false;
+
 const userForm = document.getElementById("user-form");
 const tableBody = document.getElementById("table-body");
-
-var userCount = 0;
+if (!editing) {
+  userForm.reset();
+}
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
-  // Get data from firestore.
   getUsers();
 });
+
+// Get data from firestore.
 async function getUsers() {
   const querySnapshot = await getDocs(collection(db, "users"));
   querySnapshot.forEach((doc) => {
     console.log(doc.id, "=>", doc.data());
     const user = doc.data();
     userCount++;
+
     tableBody.innerHTML += `
         <tr>
             <td> ${userCount} </td>
             <td> ${user.firstName} </td>
             <td> ${user.lastName} </td>
             <td> ${user.email} </td>
+            <td> 
+                <button class="btn btn-info btn-edit" data-id="${doc.id}">Edit</button> 
+                <button class="btn btn-danger btn-delete" data-id="${doc.id}>Delete</button> 
+            </td>
         </tr>
     `;
+  });
+
+  const btnsEdit = tableBody.querySelectorAll(".btn-edit");
+  btnsEdit.forEach((btn) => {
+    btn.addEventListener("click", async (e) => {
+      try {
+        id = e.target.dataset.id;
+        editing = true;
+        const userData = await getDoc(doc(db, "users", id));
+        const user = userData.data();
+        userForm["first-name"].value = user.firstName;
+        userForm["last-name"].value = user.lastName;
+        userForm["email"].value = user.email;
+        userForm["btn-user-form"].innerText = "Update";
+      } catch (error) {
+        console.log(error);
+      }
+    });
   });
 }
 
@@ -51,15 +84,28 @@ userForm.addEventListener("submit", async (e) => {
   const email = userForm["email"];
 
   try {
-    await addDoc(collection(db, "users"), {
-      firstName: firstName.value,
-      lastName: lastName.value,
-      email: email.value,
-    });
+    if (!editing) {
+      // Create operation.
+      await addDoc(collection(db, "users"), {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+      });
+    } else {
+      // Update operation.
+      await updateDoc(doc(db, "users", id), {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+      });
+      editing = false;
+      id = "";
+      userForm["btn-user-form"].innerText = "Save";
+    }
+    userForm.reset();
+    firstName.focus();
+    location.reload();
   } catch (error) {
     console.log(error);
   }
-  userForm.reset();
-  firstName.focus();
-  location.reload();
 });
